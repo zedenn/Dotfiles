8440ecf38696fcd52a2ca2919563b85f150288d8
zedenn <tuovisenteemu@gmail.com>
2025-11-23 20:03:59 +0200

    Minor updates

--- hypr/.config/hypr/scripts/clamshell.sh
+++ hypr/.config/hypr/scripts/clamshell.sh
@@ -9,7 +9,6 @@ if grep -q closed /proc/acpi/button/lid/LID*/state; then
     if hyprctl monitors | grep 'Monitor' | grep -v "$INTERNAL_MONITOR"; then
         # External monitor is connected, disable internal monitor
         echo "monitor = $INTERNAL_MONITOR, disable" > "$CONFIG_FILE"
-        echo "THIS MEANS THAT THE SCRIPT WAS RUN WHEN EXTERNAL DISPLAY WAS CONNECTED" > /home/zeden/hyprmonitor.txt
     else
         # No external monitor, turn off internal monitor
         hyprctl dispatch dpms off $INTERNAL_MONITOR
